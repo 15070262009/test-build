a5516604d092aadae4817f463db0efc5fd234575
wang jun <530955809@qq.com>
2020-06-23 16:06:27 +0800

    first commit

---
+++ src/config.prod.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  now: {
+    token: 'AGWk8Lf7TwnuM1TMgQnGGkjO',
+    url: 'https://ant-design-landing-build.acadsoc.vercel.app',
+    templateDir: path.join(__dirname, 'template'),
+    whiteList: ['ant-design-landing-build'],
+  },
+}
